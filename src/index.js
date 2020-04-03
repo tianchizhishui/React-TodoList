a5134fbfe2038d123fd265b07189ab7d4c049f05
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2020-04-02 20:19:40 -0700

    with css animation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList';
+import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoList />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
