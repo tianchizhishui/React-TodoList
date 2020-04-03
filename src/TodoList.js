1faca8de6a47865b8c8fd834491d324473f91f7e
Tianchi (David) Liu <55475915+tianchizhishui@users.noreply.github.com>
2020-04-02 22:34:56 -0700

    implement todoList with redux

---
+++ src/TodoList.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Input, Button, List } from 'antd';
+import store from './store';
+import 'antd/dist/antd.css';
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
+
+
+class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    console.log(this.state);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    // this.handleItemClick = this.handleItemClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
+
+  render() {
+    return (
+      <div style={{marginTop: '10px', marginLeft: '10px'}}>
+        <div>
+          <Input 
+            value={this.state.inputValue} 
+            placeholder='todo info' 
+            style={{width: '300px', marginRight: '10px'}}
+            onChange={this.handleInputChange}
+          />
+          <Button type="primary" onClick={this.handleButtonClick}>Submit</Button>
+        </div>
+        <List
+          style={{marginTop: '10px', width: '300px'}}
+          bordered
+          dataSource={this.state.list}
+          renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
+        />
+      </div>
+    )
+  }
+
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action);
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+
+  handleButtonClick() {
+    const action = getAddItemAction();
+    store.dispatch(action);
+  }
+
+  handleItemDelete(index) {
+    const action = getDeleteItemAction(index);
+    store.dispatch(action);
+  }
+}
+
+export default TodoList;
